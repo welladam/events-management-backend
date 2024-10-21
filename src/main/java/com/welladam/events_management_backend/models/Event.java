f85a22e8c4911dc5c8fb21ff4ea2859f9ac81c01
Wellington Adam <wellington.adam@soraschools.com>
2024-10-21 17:12:11 -0300

    feat: update models with lombok

--- src/main/java/com/welladam/events_management_backend/models/Event.java
+++ src/main/java/com/welladam/events_management_backend/models/Event.java
@@ -1,22 +1,35 @@
 package com.welladam.events_management_backend.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
-import java.time.LocalDateTime;
 
+import java.util.Date;
+
+@Data
 @Entity
-@Table(name = "events")
+@Table(name = "event")
 public class Event {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Title is mandatory")
     private String title;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+
+    @Column(nullable = true)
+    private String description;
+
+    @NotBlank(message = "Start Date is mandatory")
+    private Date startDate;
+    @NotBlank(message = "End Date is mandatory")
+    private Date endDate;
+
+    @Column(nullable = true)
     private Double price;
-    private Boolean isActive;
+    @Column(nullable = true)
+    private String imageUrl;
 
     @Enumerated(EnumType.STRING)
     private Status status;
@@ -25,60 +38,5 @@ public class Event {
         STARTED, COMPLETED, PAUSED
     }
 
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDateTime getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean isActive) {
-        this.isActive = isActive;
-    }
+    private Boolean isActive = true;
 }
